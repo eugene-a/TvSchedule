43991b51bff1b9eb5513b25b78c4d0721519abb2
eugene-a <eugalt@gmail.com>
2015-01-22 18:38:22 -0500

    'import m' instead of 'from m import ...'

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
-from os.path import dirname
-from setuptools import setup, find_packages
+import os.path
+import setuptools
 
 
 def read(fname):
-    with open(dirname(__file__), fname) as fp:
+    with open(os.path.irname(__file__), fname) as fp:
         fp.read()
 
 
-setup(name='tv_schedule',
-      version='0.0.1',
-      description='Generate data for ListTV',
-      author='Eugene Alterman',
-      packages=find_packages(),
-      install_requires=['httplib2', 'lxml', 'tzlocal', 'PyYAML'],
-      zip_safe=True,
-      entry_points={
-          'console_scripts': [
-              'tv_schedule=tv_schedule.schwriter:write_schedule'
-          ]
-      },
-      package_data={'tv_schedule': ['channels.txt'],
-                    'tv_schedule.source': ['sources/*.*']
-                    }
-      )
+setuptools.setup(name='tv_schedule',
+                 version='0.0.1',
+                 description='Generate data for ListTV',
+                 author='Eugene Alterman',
+                 packages=setuptools.find_packages(),
+                 install_requires=['httplib2', 'lxml', 'tzlocal', 'PyYAML'],
+                 zip_safe=True,
+                 entry_points={
+                     'console_scripts': [
+                      'tv_schedule=tv_schedule.schwriter:write_schedule'
+                     ]
+                 },
+                 package_data={'tv_schedule': ['channels.txt'],
+                               'tv_schedule.source': ['sources/*.*']
+                               }
+                 )
